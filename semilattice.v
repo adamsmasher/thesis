d4af4d2b8cc0a1b0dcb510e09611533b559a6021
Adam Smith <unfactorable.polynomial@gmail.com>
2014-11-10 20:41:35 +0100

    move lattice stuff into a separate file

---
+++ semilattice.v
@@ -0,0 +1,14 @@
+(* lattice stuff influenced by A reflection-based
+proof tactic for lattices in Coq by Daniel James
+and Ralf Hinze *)
+
+(* also: A Gentle Introducton to Type Classes and
+    Relations in Coq by Pierre Casteran *)
+    
+Class UpperSemiLattice (A : Set) := {
+  join : A -> A -> A;
+
+  join_commutative : forall a b, join a b = join b a;
+  join_associatve : forall a b c, join (join a b) c = join a (join b c);
+  join_idempotent : forall a, join a a =a
+}.
