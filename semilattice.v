bb22d92f9fa52f4e9e01ac96354f3b0d6cf89a35
Adam Smith <unfactorable.polynomial@gmail.com>
2014-12-09 19:21:59 +0100

    if we use typeclasses, this is better

--- semilattice.v
+++ semilattice.v
@@ -4,11 +4,10 @@ and Ralf Hinze *)
 
 (* also: A Gentle Introducton to Type Classes and
     Relations in Coq by Pierre Casteran *)
-    
-Class UpperSemiLattice (A : Set) := {
-  join : A -> A -> A;
 
+Class UpperSemiLattice {A : Set} (join : A -> A -> A) (bottom : A) := {
   join_commutative : forall a b, join a b = join b a;
   join_associatve : forall a b c, join (join a b) c = join a (join b c);
-  join_idempotent : forall a, join a a =a
+  join_idempotent : forall a, join a a = a;
+  bottom_prop : forall a, join bottom a = a
 }.
