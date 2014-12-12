e095a4011a2ea03b245dd67ba4118adaad49ee06
Adam Smith <unfactorable.polynomial@gmail.com>
2014-12-12 15:49:24 +0100

    complete upper semi lattice def

--- semilattice.v
+++ semilattice.v
@@ -5,9 +5,12 @@ and Ralf Hinze *)
 (* also: A Gentle Introducton to Type Classes and
     Relations in Coq by Pierre Casteran *)
 
-Class UpperSemiLattice {A : Set} (join : A -> A -> A) (bottom : A) := {
+Require Import poset.
+
+Class UpperSemiLattice {A} {precedes} (P : Poset A precedes) (join : A -> A -> A) (bottom : A) := {
   join_commutative : forall a b, join a b = join b a;
   join_associatve : forall a b c, join (join a b) c = join a (join b c);
   join_idempotent : forall a, join a a = a;
-  bottom_prop : forall a, join bottom a = a
+  bottom_prop : forall a, join bottom a = a;
+  order_induction : forall a b, precedes a b <-> join a b = b
 }.
