c61ffdf9ce0e4e95470b0f619fa33d3af0df66e2
Adam Smith <unfactorable.polynomial@gmail.com>
2015-02-17 14:06:50 +0100

    represent type system as a typeclass

--- type_system.v
+++ type_system.v
@@ -1,20 +1,16 @@
 Require Import target_calculus.
 
-Notation "s → t" := (step s t) (at level 70).
-Notation "s →@ t" := (step_ext s t) (at level 70).
-
-Parameter T : Set.
-Parameter hasType : term -> T -> Prop.
-Parameter lift_label : label -> T.
-
-Axiom subj_red :
-  forall e f t, hasType e t -> e →@ f -> hasType f t.
-
-Axiom progress :
-  forall e t, hasType e t -> exists f, e → f /\ isValue e.
-
-Axiom labels :
-  forall l m, hasType (Label l) (lift_label m) -> precedes l m.
-
-Axiom integers :
-  exists int, forall e, isValue e -> (hasType e int <-> exists k, e = Const k).
+Class TypeSystem
+    (type : Type)
+    (has_type : term -> type -> Prop)
+    (lift_label : label -> type)
+    (int : type)
+    (pair : type -> type -> type)
+:= {
+  subj_red : forall e f t, has_type e t -> full_step e f -> has_type f t;
+  progress : forall e t, has_type e t -> (exists f, cbn e f) \/ is_value e;
+  labels : forall l m, has_type (Label l) (lift_label m) -> precedes l m;
+  integers : forall v, has_type v int <-> exists k, v = Const k;
+  pairs1 : forall e f t u, has_type (Pair e f) (pair t u) -> has_type e t /\ has_type f u;
+  pairs2 : forall e f t u v, has_type e t -> has_type f u -> has_type (Pair e f) v
+}.
