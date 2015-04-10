4eda8612ccef6ab602257419cb3cee1c013cd3ec
Adam Smith <unfactorable.polynomial@gmail.com>
2015-04-10 17:17:43 +0200

    keep lines short

--- poset.v
+++ poset.v
@@ -7,5 +7,6 @@
 Class Poset (A : Set) (precedes : A -> A -> Prop) := {
   reflexivity : forall a, precedes a a;
   antisymmetry : forall a b, precedes a b -> precedes b a -> a = b;
-  transitivity : forall a b c, precedes a b -> precedes b c -> precedes a c
+  transitivity : forall a b c,
+    precedes a b -> precedes b c -> precedes a c
 }.
