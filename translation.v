455db65f2153acae4b78c5cd379a9387382ae4fb
Adam Smith <unfactorable.polynomial@gmail.com>
2015-04-16 21:17:15 +0200

    reorganize things, basically

--- translation.v
+++ translation.v
@@ -53,82 +53,6 @@ Fixpoint translation (e : source_calculus.prefix) : target_calculus.term :=
   end.
 Notation "⦇ e ⦈" := (translation e).
 
-
-Lemma translation_pair e :
-  is_term e -> (exists x, e = source_calculus.Var x /\ translation e = Var x) \/ (exists e1 e2, translation e = Pair e1 e2).
-Proof.
-  intros. destruct e ; simpl.
-  - inversion H.
-  - right. eauto.
-  - left. eauto.
-  - right. eauto.
-  - right. eauto.
-  - right. eauto.
-  - right. eauto.
-Qed.
-
-Lemma translation_closed_fst e n :
-  n_closed n e -> n_closed n (eta_fst e).
-Proof.
-  intros. destruct e ; simpl.
-  - repeat constructor.
-  - constructor.
-    + constructor.
-    + assumption.
-  - inversion H ; subst. repeat constructor ; auto.
-  - inversion H ; subst. repeat constructor ; auto.
-  - inversion H ; subst. repeat constructor ; auto.
-  - inversion H ; subst. auto.
-  - repeat constructor.
-  - repeat constructor.
-  - repeat constructor.
-  - repeat constructor.
-Qed.
-
-Lemma translation_closed_snd e n :
-  n_closed n e -> n_closed n (eta_snd e).
-Proof.
-  intros. destruct e ; simpl.
-  - repeat constructor.
-  - constructor.
-    + constructor.
-    + assumption.
-  - inversion H ; subst. repeat constructor ; auto.
-  - inversion H ; subst. repeat constructor ; auto.
-  - inversion H ; subst. repeat constructor ; auto.
-  - inversion H ; subst. auto.
-  - repeat constructor.
-  - repeat constructor.
-  - repeat constructor.
-  - repeat constructor.
-Qed.
-
-Lemma translation_closed e n :
-  source_calculus.n_closed n e -> n_closed n (translation e).
-Proof.
-  revert n. induction e ; simpl ; intros.
-  - repeat constructor.
-  - repeat constructor.
-  - constructor. ainv.
-  - ainv. constructor.
-    + constructor. now apply IHe.
-    + constructor.
-  - ainv. repeat constructor.
-    + apply translation_closed_fst. now apply IHe1.
-    + now apply IHe2.
-    + apply translation_closed_snd. now apply IHe1.
-    + apply translation_closed_fst. now apply IHe1.
-    + now apply IHe2.
-  - ainv. repeat constructor.
-    + now apply IHe.
-    + apply translation_closed_fst. now apply IHe0.
-    + now apply IHe.
-    + apply translation_closed_snd. now apply IHe0.
-  - ainv. repeat constructor.
-    + apply translation_closed_fst. now apply IHe.
-    + apply translation_closed_snd. now apply IHe.
-Qed.
-
 Inductive eta_eq : term -> term -> Prop :=
 | EtaEqR s : eta_eq s s
 | EtaEqS s t : eta_eq s t -> eta_eq t s
@@ -141,152 +65,10 @@ Inductive eta_eq : term -> term -> Prop :=
 | EtaEqLet s s' t t' : eta_eq s s' -> eta_eq t t' -> eta_eq (Let s t) (Let s' t')
 | EtaEqPair s s' t t' : eta_eq s s' -> eta_eq t t' -> eta_eq (Pair s t) (Pair s' t').
 
-
-
-
-
-Lemma fst_ren s xi :
-  ⦇s⦈.[ren xi] = ⦇s.[ren xi]⦈ -> (eta_fst ⦇s⦈).[ren xi] = eta_fst ⦇s.[ren xi]⦈.
-Proof.
-  revert xi. destruct s ; intros xi H ; try autosubst ; simpl ; inversion H ; f_equal.
-Qed.
-
-Lemma snd_ren s xi :
-  ⦇s⦈.[ren xi] = ⦇s.[ren xi]⦈ -> (eta_snd ⦇s⦈).[ren xi] = eta_snd ⦇s.[ren xi]⦈.
-Proof.
-  revert xi. destruct s ; intros xi H ; try autosubst ; simpl ; inversion H ; f_equal.
-Qed.
-
-Lemma ren_translation xi : translation >>> (subst (xi >>> ids)) = subst (xi >>> ids) >>> translation.
-Proof.
-  f_ext. intros s. simpl. revert xi. induction s ; intros ; asimpl ; autosubst_unfold ; try now autorew ;
-  repeat f_equal ; eauto using fst_ren, snd_ren.
-Qed.
-
-Hint Rewrite @ren_translation : autosubst.
-
-Lemma ren_up sigma :
-    up sigma >>> translation = up (sigma >>> translation).
-Proof. autosubst. Qed.
-
-Lemma five_one e sigma (H1 : is_term e) :
-   ⦇e⦈.[sigma >>> translation] →* ⦇e.[sigma]⦈ /\ (eta_fst ⦇e⦈).[sigma >>> translation] →* eta_fst ⦇e.[sigma]⦈ /\ (eta_snd ⦇e⦈).[sigma >>> translation] →* eta_snd ⦇e.[sigma]⦈.
-Proof.
-  revert sigma. induction e ; intros ; simpl ; eauto using step, star ; inversion H1 ; subst ; repeat split.
-  - constructor.
-  - destruct (sigma x) ; eauto using star, full_step, step.
-  - destruct (sigma x) ; eauto using star, full_step, step.
-  - apply pair_star_l. rewrite <- ren_up. apply abs_star. now apply IHe.
-  - rewrite <- ren_up. apply abs_star. now apply IHe.
-  - constructor.
-  - apply pair_star.
-    + apply app_star_r. apply app_star.
-       * now apply IHe1.
-       * now apply IHe2.
-    + apply app_star.
-       * apply app_star_r. now apply IHe1.
-       * apply app_star_r. apply app_star.
-          { now apply IHe1. }
-          { now apply IHe2. }
-  - apply app_star_r. apply app_star.
-     + now apply IHe1.
-     + now apply IHe2.
-  - apply app_star.
-     + apply app_star_r. now apply IHe1.
-     + apply app_star_r. apply app_star.
-        * now apply IHe1.
-        * now apply IHe2.
-  - apply pair_star.
-    + apply let_star.
-       * now apply IHe.
-       * rewrite <- ren_up. now apply IHe0.
-    + apply let_star.
-       * now apply IHe.
-       * rewrite <- ren_up. now apply IHe0.
-  - apply let_star.
-    + now apply IHe.
-    + rewrite <- ren_up. now apply IHe0.
-  - apply let_star.
-    + now apply IHe.
-    + rewrite <- ren_up. now apply IHe0.
-  - apply pair_star.
-    + now apply IHe.
-    + apply app_star_r. now apply IHe.
-  - now apply IHe.
-  - apply app_star_r. now apply IHe.
-Qed.
-
-Lemma five_one' e1 e2 (H1 : is_term e1) (H2 : is_term e2) :
-  ⦇e1⦈.[⦇e2⦈/] →* ⦇e1.[e2/]⦈ /\ (eta_fst ⦇e1⦈).[⦇e2⦈/] →* eta_fst ⦇e1.[e2/]⦈ /\ (eta_snd ⦇e1⦈).[⦇e2⦈/] →* eta_snd ⦇e1.[e2/]⦈.
-Proof.
-  repeat split.
-  - assert (exists sigma, ⦇e1⦈.[scons (translation e2) ids] = ⦇e1⦈.[sigma >>> translation] /\ ⦇e1.[scons e2 ids]⦈ = ⦇e1.[sigma]⦈).
-    { unfold ids. unfold Ids_term. unfold funcomp. unfold scons.
-      exists (fun x => match x with 0 => e2 | S y => source_calculus.Var y end).
-      split ; auto. repeat f_equal ; f_ext ; intros ; destruct x ; auto.
-    }
-    destruct H as [sigma [HL HR]]. autorew. now apply five_one.
-  - assert (exists sigma, (eta_fst ⦇e1⦈).[scons (translation e2) ids] = (eta_fst ⦇e1⦈).[sigma >>> translation] /\ eta_fst ⦇e1.[scons e2 ids]⦈ = eta_fst ⦇e1.[sigma]⦈).
-    { unfold ids. unfold Ids_term. unfold funcomp. unfold scons.
-      exists (fun x => match x with 0 => e2 | S y => source_calculus.Var y end).
-      split ; auto. repeat f_equal ; f_ext ; intros ; destruct x ; auto.
-    }
-    destruct H as [sigma [HL HR]]. autorew. now apply five_one.
-  - assert (exists sigma, (eta_snd ⦇e1⦈).[scons (translation e2) ids] = (eta_snd ⦇e1⦈).[sigma >>> translation] /\ eta_snd ⦇e1.[scons e2 ids]⦈ = eta_snd ⦇e1.[sigma]⦈).
-    { unfold ids. unfold Ids_term. unfold funcomp. unfold scons.
-      exists (fun x => match x with 0 => e2 | S y => source_calculus.Var y end).
-      split ; auto. repeat f_equal ; f_ext ; intros ; destruct x ; auto.
-    }
-    destruct H as [sigma [HL HR]]. autorew. now apply five_one.
-Qed.
-
 Lemma eta_pair x :
   eta_eq (Pair (eta_fst x) (eta_snd x)) x.
 Proof.
   induction x ; simpl ; eauto using eta_eq.
-Qed.
-
-Lemma simulation_beta s t (H : is_term (source_calculus.App (source_calculus.Abs s) t)) :
-  exists u, ⦇source_calculus.App (source_calculus.Abs s) t⦈ →* u /\ eta_eq u ⦇s.[t/]⦈.
-Proof.
-  ainv. simpl. esplit. split.
-  - eapply star_trans.
-    + apply pair_star.
-      * apply app_star_r. apply star_step, FullStep_step, Step_beta.
-      * apply star_step, FullStep_step, Step_neutral.
-    + eapply star_trans.
-       * apply pair_star.
-         { apply app_star_r. now apply five_one'. }
-         { apply app_star_r. apply star_step, FullStep_step, Step_beta. }
-       * apply pair_star_r. apply app_star_r. now apply five_one'.
-  - apply EtaEqEta.
-Qed.
-
-Lemma simulation_let s t (H : is_term (source_calculus.Let s t)) :
-  exists u, ( ⦇source_calculus.Let s t⦈ →* u /\ eta_eq u ⦇t.[s/]⦈).
-Proof.
-  ainv. simpl. esplit. split.
-  - eapply star_trans.
-    + apply pair_star ; apply star_step, FullStep_step, Step_let.
-    + apply pair_star ; now apply five_one'.
-  - apply eta_pair.
-Qed.
-
-Lemma simulation_label s l t (H : is_term (source_calculus.App (source_calculus.Label s l) t)) :
-  exists u, ⦇source_calculus.App (source_calculus.Label s l ) t⦈ →* u  /\ eta_eq u ⦇source_calculus.Label (source_calculus.App s t) l⦈.
-Proof.
-  ainv. simpl. repeat esplit.
-  - apply pair_star_r, star_step, FullStep_step, Step_assoc.
-  - constructor.
-Qed.
-
-Lemma simulation_step (e f : source_calculus.prefix) (He : is_term e) (Hf : is_term f) :
-  source_calculus.step e f  -> exists u, ⦇e⦈ →* u /\ eta_eq u ⦇f⦈.
-Proof.
-  intros. destruct H.
-  - now apply simulation_beta.
-  - now apply simulation_let.
-  - now apply simulation_label.
 Qed.
 
 Lemma eta_fst_app s :
@@ -403,6 +185,144 @@ Proof.
   induction 1 ; eauto using eta_eq, eq_app_snd_eta_snd.
 Qed.
 
+Lemma fst_ren s xi :
+  ⦇s⦈.[ren xi] = ⦇s.[ren xi]⦈ -> (eta_fst ⦇s⦈).[ren xi] = eta_fst ⦇s.[ren xi]⦈.
+Proof.
+  revert xi. destruct s ; intros xi H ; try autosubst ; simpl ; inversion H ; f_equal.
+Qed.
+
+Lemma snd_ren s xi :
+  ⦇s⦈.[ren xi] = ⦇s.[ren xi]⦈ -> (eta_snd ⦇s⦈).[ren xi] = eta_snd ⦇s.[ren xi]⦈.
+Proof.
+  revert xi. destruct s ; intros xi H ; try autosubst ; simpl ; inversion H ; f_equal.
+Qed.
+
+Lemma ren_translation xi : translation >>> (subst (xi >>> ids)) = subst (xi >>> ids) >>> translation.
+Proof.
+  f_ext. intros s. simpl. revert xi. induction s ; intros ; asimpl ; autosubst_unfold ; try now autorew ;
+  repeat f_equal ; eauto using fst_ren, snd_ren.
+Qed.
+
+Hint Rewrite @ren_translation : autosubst.
+
+Lemma ren_up sigma :
+    up sigma >>> translation = up (sigma >>> translation).
+Proof. autosubst. Qed.
+
+Lemma five_one e sigma (H1 : is_term e) :
+   ⦇e⦈.[sigma >>> translation] →* ⦇e.[sigma]⦈ /\ (eta_fst ⦇e⦈).[sigma >>> translation] →* eta_fst ⦇e.[sigma]⦈ /\ (eta_snd ⦇e⦈).[sigma >>> translation] →* eta_snd ⦇e.[sigma]⦈.
+Proof.
+  revert sigma. induction e ; intros ; simpl ; eauto using step, star ; inversion H1 ; subst ; repeat split.
+  - constructor.
+  - destruct (sigma x) ; eauto using star, full_step, step.
+  - destruct (sigma x) ; eauto using star, full_step, step.
+  - apply pair_star_l. rewrite <- ren_up. apply abs_star. now apply IHe.
+  - rewrite <- ren_up. apply abs_star. now apply IHe.
+  - constructor.
+  - apply pair_star.
+    + apply app_star_r. apply app_star.
+       * now apply IHe1.
+       * now apply IHe2.
+    + apply app_star.
+       * apply app_star_r. now apply IHe1.
+       * apply app_star_r. apply app_star.
+          { now apply IHe1. }
+          { now apply IHe2. }
+  - apply app_star_r. apply app_star.
+     + now apply IHe1.
+     + now apply IHe2.
+  - apply app_star.
+     + apply app_star_r. now apply IHe1.
+     + apply app_star_r. apply app_star.
+        * now apply IHe1.
+        * now apply IHe2.
+  - apply pair_star.
+    + apply let_star.
+       * now apply IHe.
+       * rewrite <- ren_up. now apply IHe0.
+    + apply let_star.
+       * now apply IHe.
+       * rewrite <- ren_up. now apply IHe0.
+  - apply let_star.
+    + now apply IHe.
+    + rewrite <- ren_up. now apply IHe0.
+  - apply let_star.
+    + now apply IHe.
+    + rewrite <- ren_up. now apply IHe0.
+  - apply pair_star.
+    + now apply IHe.
+    + apply app_star_r. now apply IHe.
+  - now apply IHe.
+  - apply app_star_r. now apply IHe.
+Qed.
+
+Lemma five_one' e1 e2 (H1 : is_term e1) (H2 : is_term e2) :
+  ⦇e1⦈.[⦇e2⦈/] →* ⦇e1.[e2/]⦈ /\ (eta_fst ⦇e1⦈).[⦇e2⦈/] →* eta_fst ⦇e1.[e2/]⦈ /\ (eta_snd ⦇e1⦈).[⦇e2⦈/] →* eta_snd ⦇e1.[e2/]⦈.
+Proof.
+  repeat split.
+  - assert (exists sigma, ⦇e1⦈.[scons (translation e2) ids] = ⦇e1⦈.[sigma >>> translation] /\ ⦇e1.[scons e2 ids]⦈ = ⦇e1.[sigma]⦈).
+    { unfold ids. unfold Ids_term. unfold funcomp. unfold scons.
+      exists (fun x => match x with 0 => e2 | S y => source_calculus.Var y end).
+      split ; auto. repeat f_equal ; f_ext ; intros ; destruct x ; auto.
+    }
+    destruct H as [sigma [HL HR]]. autorew. now apply five_one.
+  - assert (exists sigma, (eta_fst ⦇e1⦈).[scons (translation e2) ids] = (eta_fst ⦇e1⦈).[sigma >>> translation] /\ eta_fst ⦇e1.[scons e2 ids]⦈ = eta_fst ⦇e1.[sigma]⦈).
+    { unfold ids. unfold Ids_term. unfold funcomp. unfold scons.
+      exists (fun x => match x with 0 => e2 | S y => source_calculus.Var y end).
+      split ; auto. repeat f_equal ; f_ext ; intros ; destruct x ; auto.
+    }
+    destruct H as [sigma [HL HR]]. autorew. now apply five_one.
+  - assert (exists sigma, (eta_snd ⦇e1⦈).[scons (translation e2) ids] = (eta_snd ⦇e1⦈).[sigma >>> translation] /\ eta_snd ⦇e1.[scons e2 ids]⦈ = eta_snd ⦇e1.[sigma]⦈).
+    { unfold ids. unfold Ids_term. unfold funcomp. unfold scons.
+      exists (fun x => match x with 0 => e2 | S y => source_calculus.Var y end).
+      split ; auto. repeat f_equal ; f_ext ; intros ; destruct x ; auto.
+    }
+    destruct H as [sigma [HL HR]]. autorew. now apply five_one.
+Qed.
+
+Lemma simulation_beta s t (H : is_term (source_calculus.App (source_calculus.Abs s) t)) :
+  exists u, ⦇source_calculus.App (source_calculus.Abs s) t⦈ →* u /\ eta_eq u ⦇s.[t/]⦈.
+Proof.
+  ainv. simpl. esplit. split.
+  - eapply star_trans.
+    + apply pair_star.
+      * apply app_star_r. apply star_step, FullStep_step, Step_beta.
+      * apply star_step, FullStep_step, Step_neutral.
+    + eapply star_trans.
+       * apply pair_star.
+         { apply app_star_r. now apply five_one'. }
+         { apply app_star_r. apply star_step, FullStep_step, Step_beta. }
+       * apply pair_star_r. apply app_star_r. now apply five_one'.
+  - apply EtaEqEta.
+Qed.
+
+Lemma simulation_let s t (H : is_term (source_calculus.Let s t)) :
+  exists u, ( ⦇source_calculus.Let s t⦈ →* u /\ eta_eq u ⦇t.[s/]⦈).
+Proof.
+  ainv. simpl. esplit. split.
+  - eapply star_trans.
+    + apply pair_star ; apply star_step, FullStep_step, Step_let.
+    + apply pair_star ; now apply five_one'.
+  - apply eta_pair.
+Qed.
+
+Lemma simulation_label s l t (H : is_term (source_calculus.App (source_calculus.Label s l) t)) :
+  exists u, ⦇source_calculus.App (source_calculus.Label s l ) t⦈ →* u  /\ eta_eq u ⦇source_calculus.Label (source_calculus.App s t) l⦈.
+Proof.
+  ainv. simpl. repeat esplit.
+  - apply pair_star_r, star_step, FullStep_step, Step_assoc.
+  - constructor.
+Qed.
+
+Lemma simulation_step (e f : source_calculus.prefix) (He : is_term e) (Hf : is_term f) :
+  source_calculus.step e f  -> exists u, ⦇e⦈ →* u /\ eta_eq u ⦇f⦈.
+Proof.
+  intros. destruct H.
+  - now apply simulation_beta.
+  - now apply simulation_let.
+  - now apply simulation_label.
+Qed.
+
 Lemma simulation (e f : source_calculus.prefix) (He : is_term e) (Hf : is_term f) :
   e → f  -> exists u, ⦇e⦈  →* u /\ eta_eq u ⦇f⦈.
 Proof.
@@ -436,5 +356,81 @@ Proof.
        * apply app_star_r. apply eta_snd_star. eassumption.
     + apply EtaEqPair ; eauto using eta_eq, eta_eq_fst, eta_eq_snd.
 Qed.
+
+(*
+Lemma translation_pair e :
+  is_term e -> (exists x, e = source_calculus.Var x /\ translation e = Var x) \/ (exists e1 e2, translation e = Pair e1 e2).
+Proof.
+  intros. destruct e ; simpl.
+  - inversion H.
+  - right. eauto.
+  - left. eauto.
+  - right. eauto.
+  - right. eauto.
+  - right. eauto.
+  - right. eauto.
+Qed.
+
+Lemma translation_closed_fst e n :
+  n_closed n e -> n_closed n (eta_fst e).
+Proof.
+  intros. destruct e ; simpl.
+  - repeat constructor.
+  - constructor.
+    + constructor.
+    + assumption.
+  - inversion H ; subst. repeat constructor ; auto.
+  - inversion H ; subst. repeat constructor ; auto.
+  - inversion H ; subst. repeat constructor ; auto.
+  - inversion H ; subst. auto.
+  - repeat constructor.
+  - repeat constructor.
+  - repeat constructor.
+  - repeat constructor.
+Qed.
+
+Lemma translation_closed_snd e n :
+  n_closed n e -> n_closed n (eta_snd e).
+Proof.
+  intros. destruct e ; simpl.
+  - repeat constructor.
+  - constructor.
+    + constructor.
+    + assumption.
+  - inversion H ; subst. repeat constructor ; auto.
+  - inversion H ; subst. repeat constructor ; auto.
+  - inversion H ; subst. repeat constructor ; auto.
+  - inversion H ; subst. auto.
+  - repeat constructor.
+  - repeat constructor.
+  - repeat constructor.
+  - repeat constructor.
+Qed.
+
+Lemma translation_closed e n :
+  source_calculus.n_closed n e -> n_closed n (translation e).
+Proof.
+  revert n. induction e ; simpl ; intros.
+  - repeat constructor.
+  - repeat constructor.
+  - constructor. ainv.
+  - ainv. constructor.
+    + constructor. now apply IHe.
+    + constructor.
+  - ainv. repeat constructor.
+    + apply translation_closed_fst. now apply IHe1.
+    + now apply IHe2.
+    + apply translation_closed_snd. now apply IHe1.
+    + apply translation_closed_fst. now apply IHe1.
+    + now apply IHe2.
+  - ainv. repeat constructor.
+    + now apply IHe.
+    + apply translation_closed_fst. now apply IHe0.
+    + now apply IHe.
+    + apply translation_closed_snd. now apply IHe0.
+  - ainv. repeat constructor.
+    + apply translation_closed_fst. now apply IHe.
+    + apply translation_closed_snd. now apply IHe.
+Qed.*)
 
 End Translation.
